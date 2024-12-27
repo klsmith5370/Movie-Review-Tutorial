84194d1f502c688c4471889711216f307debe093
Kourtney Smith <klsmith5370@gmail.com>
2024-12-27 14:56:38 -0500

    Updated control to map it directly to API

--- movies/src/main/java/movie/reviews/movies/MovieController.java
+++ movies/src/main/java/movie/reviews/movies/MovieController.java
@@ -1,5 +1,10 @@
 package movie.reviews.movies;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/movies")
 public class MovieController {
     
 }
