58618042aca80c1f137625997d4eb6d20cfef111
Kourtney Smith <klsmith5370@gmail.com>
2025-01-07 11:36:53 -0500

    Adding API for getting single movie

--- movies/src/main/java/movie/reviews/movies/MovieController.java
+++ movies/src/main/java/movie/reviews/movies/MovieController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,14 @@ import java.util.List;
 public class MovieController {
     @Autowired
     private MovieService movieService;
+
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getSingleMovie(@PathVariable ObjectId id) {
+        return
     }
 }
