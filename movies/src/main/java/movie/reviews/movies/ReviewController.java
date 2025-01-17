9a444fc530719d594444093f9778460b5724ffd2
Kourtney Smith <klsmith5370@gmail.com>
2025-01-17 14:29:44 -0500

    Setting up a review controller for the post request

---
+++ movies/src/main/java/movie/reviews/movies/ReviewController.java
@@ -0,0 +1,10 @@
+package movie.reviews.movies;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vl/movies")
+public class ReviewController {
+    
+}
