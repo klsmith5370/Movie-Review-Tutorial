6693452c61682b99463e28a4a14d9e1ce3a0736d
Kourtney Smith <klsmith5370@gmail.com>
2025-01-17 18:13:40 -0500

    Tested with postman, took out code that was causing an error

--- movies/src/main/java/movie/reviews/movies/ReviewService.java
+++ movies/src/main/java/movie/reviews/movies/ReviewService.java
@@ -17,7 +17,7 @@ public class ReviewService {
     public Review createReview(String reviewBody, String imdbId) {
         Review review = reviewRepository.insert(new Review(reviewBody));
 
-        reviewRepository.insert(review);
+        
         mongoTemplate.update(Movie.class)
             .matching(Criteria.where("imdbId").is(imdbId))
             .apply(new Update().push("reviewIds").value(review))
