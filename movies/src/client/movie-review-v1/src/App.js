864c21c258208da58b2d78d3ba37b563d8db0ac7
Kourtney Smith <klsmith5370@gmail.com>
2025-01-29 18:45:05 -0500

    Updated axios config and updated movie controller to fix error with retreiving data

--- movies/src/client/movie-review-v1/src/App.js
+++ movies/src/client/movie-review-v1/src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   useEffect(() => {
     getMovies();
-  }, [])
+  },[])
 
   return (
     <div className="App">
--- movies/src/main/java/movie/reviews/movies/MovieController.java
+++ movies/src/main/java/movie/reviews/movies/MovieController.java
@@ -3,6 +3,7 @@ package movie.reviews.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
