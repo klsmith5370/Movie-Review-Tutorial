9da806b1d6d4c49f3315ccab79da79f18039ce96
Kourtney Smith <klsmith5370@gmail.com>
2025-01-30 16:15:43 -0500

    Created Layout component and added routing functionality

--- movies/src/client/movie-review-v1/src/App.js
+++ movies/src/client/movie-review-v1/src/App.js
@@ -1,6 +1,8 @@
 // import "./App.css";
 import api from "./api/axiosConfig";
 import {useState, useEffect} from "react";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
@@ -10,7 +12,6 @@ function App() {
 
     try {
       const response = await api.get("/api/v1/movies");
-      console.log(response.data);
       setMovies(response.data);
 
     } catch (error) {
@@ -25,7 +26,11 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={Layout}>
+
+        </Route>
+      </Routes>
     </div>
   );
 }
