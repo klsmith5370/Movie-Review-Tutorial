399ed5b23f20a33dce1479a50d402fb2af910548
Kourtney Smith <klsmith5370@gmail.com>
2025-02-17 18:38:41 -0500

    Created component folder for Header component, added base layout to get started

---
+++ movies/src/client/movie-review-v1/src/components/header/Header.js
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+
+const Header = () => {
+    return (
+        <div>
+
+        </div>
+
+    )
+};
+
+export default Header;
