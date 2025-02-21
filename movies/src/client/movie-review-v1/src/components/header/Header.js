3df9004e22c8fbe5babf5ec5adb887ec856bfb23
Kourtney Smith <klsmith5370@gmail.com>
2025-02-21 11:31:02 -0500

    Added nav links for nav bar and buttons for log in and registration functionality

--- movies/src/client/movie-review-v1/src/components/header/Header.js
+++ movies/src/client/movie-review-v1/src/components/header/Header.js
@@ -14,7 +14,19 @@ const Header = () => {
 
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
-                
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: "100px" }}
+                        navbarScroll
+                    >
+                    <NavLink className="nav-link" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/watchlist">Watch List</NavLink>
+
+                    </Nav>
+                    <Button variant="outline-into" className="me-2">Login</Button>
+                    <Button variant="outline-info" className="me-2">Register</Button>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
 
