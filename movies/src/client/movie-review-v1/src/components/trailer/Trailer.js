b30cd0680cfc9c1ec1bfb4d6fb8a8ff5dbb627a5
Kourtney Smith <klsmith5370@gmail.com>
2025-02-28 14:30:25 -0500

    Created layout for trailer component

---
+++ movies/src/client/movie-review-v1/src/components/trailer/Trailer.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import "./Trailer.css";
+
+import React from "react";
+
+const Trailer = () => {
+    return (
+        <div></div>
+    )
+};
+
+export default Trailer;
