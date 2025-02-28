08ccb07de3bd848baf578d6297777691e9b26634
Kourtney Smith <klsmith5370@gmail.com>
2025-02-28 14:35:46 -0500

    Added in container and controls when trailer is played

--- movies/src/client/movie-review-v1/src/components/trailer/Trailer.js
+++ movies/src/client/movie-review-v1/src/components/trailer/Trailer.js
@@ -5,8 +5,20 @@ import "./Trailer.css";
 import React from "react";
 
 const Trailer = () => {
+
+    let params = useParams();
+    const key = params.ytTrailerId;
+
     return (
-        <div></div>
+        <div className="react-player-container">
+            {(key!=null) ? <ReactPlayer 
+            controls="true" 
+            playing={true} 
+            url={`https://www.youtube.com/watch?v=${key}`}
+            width="100%"
+            height="100%" 
+            /> : null}
+        </div>
     )
 };
 
