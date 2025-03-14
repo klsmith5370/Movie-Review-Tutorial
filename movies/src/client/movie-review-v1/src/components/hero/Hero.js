1862b51c8760e83d46f1de7a7ae86a5d8e63bcdf
Kourtney Smith <klsmith5370@gmail.com>
2025-03-14 19:36:21 -0400

    Finished trailer component with styling

--- movies/src/client/movie-review-v1/src/components/hero/Hero.js
+++ movies/src/client/movie-review-v1/src/components/hero/Hero.js
@@ -4,6 +4,7 @@ import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 
 const Hero = ({movies}) => {
@@ -26,9 +27,12 @@ const Hero = ({movies}) => {
                                             </div>
 
                                             <div className="movie-buttons-container">
-                                                <div className="play-button-icon-container">
-                                                    <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay} />
-                                                </div>
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                    <div className="play-button-icon-container">
+                                                        <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay} />
+                                                    </div>
+                                                </Link>
+                                                
                                             </div>
                                         </div>
                                     </div>
