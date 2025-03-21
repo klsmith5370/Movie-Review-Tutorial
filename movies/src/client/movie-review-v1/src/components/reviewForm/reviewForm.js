bdb42447fdb12ad70e6fb9d745afc4ed91df8068
Kourtney Smith <klsmith5370@gmail.com>
2025-03-21 11:52:06 -0400

    Added form for writing out reviews

--- movies/src/client/movie-review-v1/src/components/reviewForm/reviewForm.js
+++ movies/src/client/movie-review-v1/src/components/reviewForm/reviewForm.js
@@ -1,10 +1,14 @@
 import { Form, Button } from "react-bootstrap";
 
-const ReviewForm = () => {
+const ReviewForm = ({ handleSubmit, revText, labelText, defaultValue }) => {
     return (
-        <div>
-
-        </div>
+        <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>{labelText}</Form.Label>
+                <Form.Control ref={revText} as="textarea" rows={3} defaultValue={defaultValue} />
+            </Form.Group>
+            <Button variant="outline-info">Submit</Button>
+        </Form>
     )
 }
 
