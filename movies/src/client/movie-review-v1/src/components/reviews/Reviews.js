ed831d91f8b09ed05204df645f681841dd20a939
Kourtney Smith <klsmith5370@gmail.com>
2025-03-25 19:04:04 -0400

    Created reviews file for reviews component

---
+++ movies/src/client/movie-review-v1/src/components/reviews/Reviews.js
@@ -0,0 +1,17 @@
+import { useEfect, useRef } from "react";
+import api from "../../api/axiosConfig";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import ReviewForm from "../reviewForm/reviewForm";
+
+import React from "react";
+
+const Reviews = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Reviews;
