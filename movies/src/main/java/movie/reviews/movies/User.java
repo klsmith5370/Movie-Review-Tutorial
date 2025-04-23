e5b9a6f576bc07c77a670911725a3406255401a9
Kourtney Smith <klsmith5370@gmail.com>
2025-04-23 19:53:25 -0400

    Started creating user model

--- movies/src/main/java/movie/reviews/movies/User.java
+++ movies/src/main/java/movie/reviews/movies/User.java
@@ -3,6 +3,11 @@ package movie.reviews.movies;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document (collection = "users")
+
 public class User {
-    
+    @Id
+    private String id;
+    private String username;
+    private String email;
 }
