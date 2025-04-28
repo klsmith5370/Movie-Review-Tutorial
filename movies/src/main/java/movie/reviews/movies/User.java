e4fc6dc549baa5a2003ad4f7c9fce94d81035714
Kourtney Smith <klsmith5370@gmail.com>
2025-04-28 19:49:54 -0400

    Added getters and setters for user model

--- movies/src/main/java/movie/reviews/movies/User.java
+++ movies/src/main/java/movie/reviews/movies/User.java
@@ -17,4 +17,21 @@ public class User {
         this.username = username;
         this.email = email;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
