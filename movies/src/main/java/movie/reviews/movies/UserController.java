9d4bcece3e7e9db161da040531c338c898019087
Kourtney Smith <klsmith5370@gmail.com>
2025-05-06 19:31:12 -0400

    Created controller for users

---
+++ movies/src/main/java/movie/reviews/movies/UserController.java
@@ -0,0 +1,14 @@
+package movie.reviews.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    
+}
