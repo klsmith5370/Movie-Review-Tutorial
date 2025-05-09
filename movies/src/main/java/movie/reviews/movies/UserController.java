0a60ce24b63963316b189c0f9fe74785cbde62ae
Kourtney Smith <klsmith5370@gmail.com>
2025-05-08 20:40:45 -0400

    Finished setting up user controller

--- movies/src/main/java/movie/reviews/movies/UserController.java
+++ movies/src/main/java/movie/reviews/movies/UserController.java
@@ -10,5 +10,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-    
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody Map<String, String> payload) {
+        String username = payload.get("username");
+        String email = payload.get("email");
+        return new ResponseEntity<>(userService.createUser(username, email), HttpStatus.CREATED);
+    }
 }
