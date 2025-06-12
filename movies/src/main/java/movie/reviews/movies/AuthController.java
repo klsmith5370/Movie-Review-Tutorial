fdd1657c6ec4c6d3ddf2c217a3acaf2727d47734
Kourtney Smith <klsmith5370@gmail.com>
2025-06-11 22:22:38 -0400

    Added auth service for backend, still working on code for auth service

---
+++ movies/src/main/java/movie/reviews/movies/AuthController.java
@@ -0,0 +1,7 @@
+package movie.reviews.movies;
+
+
+
+public class AuthController {
+    
+}
---
+++ movies/src/main/java/movie/reviews/movies/AuthService.java
@@ -0,0 +1,18 @@
+package movie.reviews.movies;
+
+import movie.reviews.movies.User;
+import movie.reviews.movies.UserRepository;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    
+}
